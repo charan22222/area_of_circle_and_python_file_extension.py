bb2f1bafabeefb62f20771aa8558a57c7102810e
charan22222 <77820282+charan22222@users.noreply.github.com>
2021-07-12 10:15:58 +0530

    Create radius_filename.py

---
+++ radius_filename.py
@@ -0,0 +1,10 @@
+#Area of circle
+PI = 3.14
+radius = 1.1
+Area = PI*radius*radius
+Print("Area of circle = %2f",area)
+
+#python extension
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
